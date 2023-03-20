e68fdeeefd70c6c8f383e854e6d5d3cbc99ba9c9
banahaker <pn0818x@outlook.com>
2023-03-20 18:55:09 +0800

    ch4

---
+++ CH.4 函數/hello.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void hello() {
+  cout << "hello" << endl;
+}
+
+int main() {
+  hello();
+  return 0;
+}
---
+++ CH.4 函數/return.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+bool f() {
+  return false;
+}
+
+int main(){
+  cout << f() << endl;
+  return 0;
+}
